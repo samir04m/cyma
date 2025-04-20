94c8dae96230ad7b6d8561edaa9604b20a5e3ada
Samir Mejia <samir04m.dev@gmail.com>
2025-04-20 02:41:31 -0500

    Validación de formulario

--- main/views.py
+++ main/views.py
@@ -8,8 +8,14 @@ def Home(request):
     return render(request, 'main/home.html', {})
 
 def Questionnaire(request):
+    name = request.GET.get('name', '')
+    if not name:
+        return redirect('main:Home')
+
+    context = {'name': name}
+
     if request.method == 'POST':
         print(request.POST)
         return redirect('main:Questionnaire')
 
-    return render(request, 'main/questionnaire.html', {})
+    return render(request, 'main/questionnaire.html', context)
